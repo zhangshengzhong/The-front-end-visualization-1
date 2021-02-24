d304a047c7cbe3da62554228e8e9c969e3520640
zhangshengzhong <1799704667@qq.com>
2021-02-24 16:40:35 +0800

    引入element

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
   <div id="app">
-    hello world
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+    </div>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
   }
 }
 </script>
-<style src="./init/normalize.css"></style>
-<style lang="scss">
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
